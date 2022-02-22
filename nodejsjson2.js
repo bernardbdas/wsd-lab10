9ef95881d345e590ddba1b6a3d8ea5b524b59253
bernardbdas <bernarddas1@gmail.com>
2022-02-23 00:24:48 +0530

    commit

--- nodejsjson2.js
+++ nodejsjson2.js
@@ -1,8 +1,12 @@
 const http = require('http')
+const cors = require('cors')
 const { load2 } = require('./nodejsjson.js');
 const express = require('express')
 const app = express();
 const port = 5200;
+app.use(cors({
+    origin: "*",
+}))
 app.listen(port, () => console.log("Server is Running.."));
 app.get('/user', (req, res) => {
     res.send(load2())
